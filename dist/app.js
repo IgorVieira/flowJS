7f61c6b9bd403009525907446c3b77efaf482cdb
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-09-30 02:18:22 -0300

    existence validation

--- dist/app.js
+++ dist/app.js
@@ -14,17 +14,27 @@ var _construction = require('./construction');
 
 var _construction2 = _interopRequireDefault(_construction);
 
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
 var rl = _readline2['default'].createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question("name project:", function (app) {
+rl.question('\x1b[36mName project\x1b[0m : ', function (app) {
 
   var doProject = new _construction2['default']();
 
-  doProject.createProject(app);
-  doProject.createModules(app);
+  _fs2['default'].exists(app, function (err) {
+    if (!err) {
+      doProject.createProject(app);
+      doProject.createModules(app);
+    } else {
+      console.log('Folder exists');
+    }
+  });
 
   rl.close();
 });

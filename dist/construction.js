8de0c79401933a0d16d1d070dc11b1dda5b77db8
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-09-29 23:56:18 -0300

    use arrow functions and remove bad function

--- dist/construction.js
+++ dist/construction.js
@@ -9,39 +9,12 @@ var _mkdirp2 = _interopRequireDefault(_mkdirp);
 var doProject = function doProject() {
 
   this.createProject = function (app) {
-
-    return (0, _mkdirp2['default'])('./' + app + '', function (err) {
+    return (0, _mkdirp2['default'])(app, function (err) {
       if (err) console.log(err);else console.log('flowJS create:' + app);
     });
   };
 
-  /*  
-     let modules = [ 
-          {block:'controllers'},
-          {block:'models'},
-          {block:'routes'}
-      ];  
-          
-      let blocks = modules.reduce(function(modules){
-          return mkdirp('./'+app+'/'+blocks,(err => {
-           if(err) console.log(err)
-           else console.log('flowJS create:'+blocks);
-         }));
-      });
-  */
-
-  this.createModules = function (app) {
-
-    var index = undefined;
-    var fruits = ["Banana", "Orange", "Apple", "Mango"];
-    for (index = 0; index < 4; index++) {
-      //     let blocks += fruits[index];
-
-      return (0, _mkdirp2['default'])('./' + app + '/' + fruits[index], function (err) {
-        if (err) console.log(err);else console.log('flowJS create:' + fruits[index]);
-      });
-    }
-  };
+  this.createModules = function (app) {};
 };
 
 module.exports = doProject;
--- src/construction.js
+++ src/construction.js
@@ -2,48 +2,18 @@ import mkdirp from 'mkdirp';
 
 let doProject = function() {
 
-  this.createProject = function(app){
-   
-   return mkdirp('./'+app+'',(err => {
+  this.createProject = (app =>{   
+   return mkdirp(app,(err => {
        if(err) console.log(err)
        else console.log('flowJS create:'+app);          
     }));
-  };
-
-/*  
-   let modules = [ 
-        {block:'controllers'},
-        {block:'models'},
-        {block:'routes'}
-    ];  
-        
-    let blocks = modules.reduce(function(modules){
-        return mkdirp('./'+app+'/'+blocks,(err => {
-         if(err) console.log(err)
-         else console.log('flowJS create:'+blocks);
-       }));
-    });
-*/
+  });
 
 
 
-  this.createModules = function(app){
-   
-    let index;
-    let fruits = ["Banana", "Orange", "Apple", "Mango"];
-    for	(index = 0; index < 4; index++) {
-//     let blocks += fruits[index];
-
-	
-         return mkdirp('./'+app+'/'+fruits[index],(err => {
-          if(err) console.log(err)
-          else console.log('flowJS create:'+fruits[index]);
-         }));
-
-    
-       	
-     } 
- }
+  this.createModules = (app => {
+ 
+  })
 
 }
 

57934f11ab0f30312546bcdca0923240bd8dc8ff
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-11-23 03:26:23 -0200

    change functions

--- src/construction.js
+++ src/construction.js
@@ -1,49 +1,50 @@
 import mkdirp from 'mkdirp';
 import fs from 'fs';
 
-
 let doProject = function() {
 
   /*create a folder project*/
 
 
-  this.createProject = (app =>{   
+  this.createProject = (app =>{
    return mkdirp(app,(err => {
        if(err) console.log(err)
-       else console.log('   \x1b[36mFlow start app\x1b[0m : '+app);          
+       else console.log('   \x1b[36mFlow start app\x1b[0m : '+app);
     }));
    complete();
   });
 
  /*create a subfolders for a project*/
 
-  this.createModules = (app => {  
+  this.createModules = (app => {
 
-    function create(app){   
+    function create(app){
 
-        mkdirp(app+'/app',() => {
-             console.log('   \x1b[35mcreate\x1b[0m : ' + '/app'); 
-        })
-        mkdirp(app+'/config',() => {
-              console.log('   \x1b[35mcreate\x1b[0m : ' + '/config');    
-        })
-        mkdirp(app+'/public',() => {
-              console.log('   \x1b[35mcreate\x1b[0m : ' + '/public');    
-        })
-        mkdirp(app+'/test',() => {
-              console.log('   \x1b[35mcreate\x1b[0m : ' + '/test'); 
-        })
+
+      var folders = [
+          {name:'app'}
+          ,{name:'config'}
+          ,{name:'public'}
+        ]
+
+      for (var i = 0; i < folders.length; i++) {
+        mkdirp(app+'/'+folders[i].name,(err => {
+          if (err) console.error(err)
+        }))
+       console.log('   \x1b[35mcreate\x1b[0m : ' + folders[i].name)
+      }
+
 
     }
 
     return create(app)
-    complete();    
-    
+    complete();
+
   });
 
 
 
-  
+
 }
 
 module.exports = doProject;
